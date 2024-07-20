8689667924a2b6190ce8adbfbd4d7235bcb92d2f
Ourtbitch <misaelchallco0@gmail.com>
2024-07-20 00:18:00 -0500

    estructura

--- src/Cadenas.java
+++ src/Cadenas.java
@@ -1,3 +1,11 @@
 public class Cadenas {
+    public String texto;
+    /*Problema: Escribe una función que reciba una cadena de texto
+     y devuelva el número de palabras que tienen más de 3 caracteres
+      y que no contengan ninguna vocal.
+    Ejemplo: Para la cadena "brrr grrrt text hello", la función debería 
+    devolver 2, ya que "brrr" y "grrrt" son las únicas palabras que 
+    cumplen con las condiciones. */
 
+    
 }
